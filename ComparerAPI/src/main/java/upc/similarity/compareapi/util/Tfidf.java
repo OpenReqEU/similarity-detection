cd88191b1d38c68420a0e87aaa977db0fb01c132
Ferran Agulló López <fewrnap@gmail.com>
2019-06-17 15:27:02 +0200

    updates

--- ComparerAPI/src/main/java/upc/similarity/compareapi/util/Tfidf.java
+++ ComparerAPI/src/main/java/upc/similarity/compareapi/util/Tfidf.java
@@ -7,6 +7,8 @@ import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import upc.similarity.compareapi.config.Control;
 import upc.similarity.compareapi.exception.InternalErrorException;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
@@ -43,17 +45,119 @@ public class Tfidf {
             wordBag.add(tf(doc,corpusFrequency));
         }
         int i = 0;
+        long total = 0;
+        long i_0 = 0;
+        long i_1 = 0;
+        long i_2 = 0;
+        long i_3 = 0;
+        long i_4 = 0;
+        long i_5 = 0;
+        long i_6 = 0;
+        long i_7 = 0;
+        long i_8 = 0;
+        long i_9 = 0;
+        long i_10 = 0;
         for (List<String> doc : docs) {
             HashMap<String, Double> aux = new HashMap<>();
             for (String s : doc) {
                 Double idf = idf(docs.size(), corpusFrequency.get(s));
                 Integer tf = wordBag.get(i).get(s);
                 double tfidf = idf * tf;
+                ++total;
                 if (tfidf>=cutoffParameter) aux.put(s, tfidf);
+                int value = (int) Math.floor(tfidf);
+                switch (value) {
+                    case 0:
+                        ++i_1;
+                        ++i_2;
+                        ++i_3;
+                        ++i_4;
+                        ++i_5;
+                        ++i_6;
+                        ++i_7;
+                        ++i_8;
+                        ++i_9;
+                        ++i_10;
+                    case 1:
+                        ++i_2;
+                        ++i_3;
+                        ++i_4;
+                        ++i_5;
+                        ++i_6;
+                        ++i_7;
+                        ++i_8;
+                        ++i_9;
+                        ++i_10;
+                        break;
+                    case 2:
+                        ++i_3;
+                        ++i_4;
+                        ++i_5;
+                        ++i_6;
+                        ++i_7;
+                        ++i_8;
+                        ++i_9;
+                        ++i_10;
+                        break;
+                    case 3:
+                        ++i_4;
+                        ++i_5;
+                        ++i_6;
+                        ++i_7;
+                        ++i_8;
+                        ++i_9;
+                        ++i_10;
+                        break;
+                    case 4:
+                        ++i_5;
+                        ++i_6;
+                        ++i_7;
+                        ++i_8;
+                        ++i_9;
+                        ++i_10;
+                        break;
+                    case 5:
+                        ++i_6;
+                        ++i_7;
+                        ++i_8;
+                        ++i_9;
+                        ++i_10;
+                        break;
+                    case 6:
+                        ++i_7;
+                        ++i_8;
+                        ++i_9;
+                        ++i_10;
+                        break;
+                    case 7:
+                        ++i_8;
+                        ++i_9;
+                        ++i_10;
+                        break;
+                    case 8:
+                        ++i_9;
+                        ++i_10;
+                        break;
+                    case 9:
+                        ++i_10;
+                        break;
+                }
             }
             tfidfComputed.put(corpus.get(i),aux);
             ++i;
         }
+        System.out.println("total: " + total);
+        System.out.println(i_0);
+        System.out.println(i_1);
+        System.out.println(i_2);
+        System.out.println(i_3);
+        System.out.println(i_4);
+        System.out.println(i_5);
+        System.out.println(i_6);
+        System.out.println(i_7);
+        System.out.println(i_8);
+        System.out.println(i_9);
+        System.out.println(i_10);
         return tfidfComputed;
     }
 
